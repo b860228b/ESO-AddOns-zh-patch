28747bb78564e9b59aa93ef6a706b1f03d7d9ca9
ImRyckDer <b860228b@gmail.com>
2018-08-14 23:24:16 +0800

    Update zh.lua

--- SkyShards/Language/zh.lua
+++ SkyShards/Language/zh.lua
@@ -8,44 +8,57 @@
 
 
 --tooltips
-SafeAddString(SKYS_KNOWN,                 "Collected", 1)
+SafeAddString(SKYS_KNOWN,                 "已收集", 1)
 
-SafeAddString(SKYS_MOREINFO1,             "Town", 1)
+SafeAddString(SKYS_MOREINFO1,             "城镇", 1)
 SafeAddString(SKYS_MOREINFO2,             "Solo dungeon", 1)
 SafeAddString(SKYS_MOREINFO3,             "Public dungeon", 1)
 SafeAddString(SKYS_MOREINFO4,             "Under ground", 1)
 SafeAddString(SKYS_MOREINFO5,             "Group Delve", 1)
 
+SafeAddString(SKYS_SET_WAYPOINT,          "将航点设置为天空碎片", 1)
+
 --settings menu header
-SafeAddString(SKYS_TITLE,                 "SkyShards", 1)
+SafeAddString(SKYS_TITLE,                 "天空碎片", 1)
 
 --appearance
-SafeAddString(SKYS_PIN_TEXTURE,           "Select map pin icons", 1)
-SafeAddString(SKYS_PIN_TEXTURE_DESC,      "Select map pin icons.", 1)
-SafeAddString(SKYS_PIN_SIZE,              "Pin size", 1)
-SafeAddString(SKYS_PIN_SIZE_DESC,         "Set the size of the map pins.", 1)
-SafeAddString(SKYS_PIN_LAYER,             "Pin layer", 1)
-SafeAddString(SKYS_PIN_LAYER_DESC,        "Set the layer of the map pins", 1)
+SafeAddString(SKYS_PIN_TEXTURE,           "选择地图图钉图案", 1)
+SafeAddString(SKYS_PIN_TEXTURE_DESC,      "选择地图图钉图案。", 1)
+SafeAddString(SKYS_PIN_SIZE,              "图钉大小", 1)
+SafeAddString(SKYS_PIN_SIZE_DESC,         "设置地图图钉的大小。", 1)
+SafeAddString(SKYS_PIN_LAYER,             "图钉图层", 1)
+SafeAddString(SKYS_PIN_LAYER_DESC,        "当地图圖釘处于与其他坐标相同的坐标时，设置它们的图层。", 1)
 
 --compass
-SafeAddString(SKYS_COMPASS_UNKNOWN,       "Show skyshards on the compass.", 1)
-SafeAddString(SKYS_COMPASS_UNKNOWN_DESC,  "Show/hide icons for uncollected skyshards on the compass.", 1)
-SafeAddString(SKYS_COMPASS_DIST,          "Max pin distance", 1)
-SafeAddString(SKYS_COMPASS_DIST_DESC,     "The maximum distance for pins to appear on the compass.", 1)
+SafeAddString(SKYS_COMPASS_UNKNOWN,       "在罗盘上显示天空碎片。", 1)
+SafeAddString(SKYS_COMPASS_UNKNOWN_DESC,  "在罗盘上显示/隐藏未收集的天空碎片的图标。", 1)
+SafeAddString(SKYS_COMPASS_DIST,          "最大图钉距离", 1)
+SafeAddString(SKYS_COMPASS_DIST_DESC,     "图钉出现在罗盘上的最大距离。", 1)
 
-SafeAddString(SKYS_SKILLS						, "Recap on the skill panel", 1)
-SafeAddString(SKYS_SKILLS_DESC				, "Select the display format of the SkyShards count on the skill panel", 1)
-SafeAddString(SKYS_SKILLS_OPTION1			, "Basic", 1)
-SafeAddString(SKYS_SKILLS_OPTION3			, "Advanced", 1)
-SafeAddString(SKYS_SKILLS_OPTION2			, "Detailed", 1)
+SafeAddString(SKYS_MAINWORLD,             "在世界地图上的天空碎片图钉颜色", 1)
+SafeAddString(SKYS_MAINWORLD_DESC,        "在世界地图上直接可见的图钉颜色", 1)
+
+--skill panel
+SafeAddString(SKYS_SKILLS,                "翻新技能面板", 1)
+SafeAddString(SKYS_SKILLS_DESC,           "选择在技能面板裡的天空碎片计数的显示格式", 1)
+SafeAddString(SKYS_SKILLS_OPTION1,        "基本", 1)
+SafeAddString(SKYS_SKILLS_OPTION3,        "高级", 1)
+SafeAddString(SKYS_SKILLS_OPTION2,        "详细", 1)
 
 --filters
-SafeAddString(SKYS_UNKNOWN,               "Show unknown skyshards", 1)
-SafeAddString(SKYS_UNKNOWN_DESC,          "Show/hide icons for unknown skyshards on the map.", 1)
-SafeAddString(SKYS_COLLECTED,             "Show collected skyshards", 1)
-SafeAddString(SKYS_COLLECTED_DESC,        "Show/hide icons for already collected skyshards on the map.", 1)
+SafeAddString(SKYS_UNKNOWN,               "显示未知的天空碎片", 1)
+SafeAddString(SKYS_UNKNOWN_DESC,          "在地图上显示/隐藏未知的天空碎片的图标。", 1)
+SafeAddString(SKYS_COLLECTED,             "显示收集的天空碎片", 1)
+SafeAddString(SKYS_COLLECTED_DESC,        "在地图上显示/隐藏已收集的天空碎片的图标。", 1)
 
 --worldmap filters
-SafeAddString(SKYS_FILTER_UNKNOWN,        "Unknown skyshards", 1)
-SafeAddString(SKYS_FILTER_COLLECTED,      "Collected skyshards", 1)
+SafeAddString(SKYS_FILTER_UNKNOWN,        "未知的天空碎片", 1)
+SafeAddString(SKYS_FILTER_COLLECTED,      "收集的天空碎片", 1)
 
+-- Immersive Mode
+SafeAddString(SKYS_IMMERSIVE              "启用沉浸模式基于...", 1)
+SafeAddString(SKYS_IMMERSIVE_DESC         "根据你所查看的当前区域的以下任務的完成情况，将不会显示未知的天空碎片", 1)
+
+SafeAddString(SKYS_IMMERSIVE_CHOICE1      "禁用", 1)
+SafeAddString(SKYS_IMMERSIVE_CHOICE2      "区域主線任务", 1)
+SafeAddString(SKYS_IMMERSIVE_CHOICE5      "区域任务", 1)
