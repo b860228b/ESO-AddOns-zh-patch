177043e25b96178c87ba78f17d9c030719be1352
ImRyckDer <b860228b@gmail.com>
2018-08-15 16:45:57 +0800

    Add files via upload

--- Lorebooks/Language/zh.lua
+++ Lorebooks/Language/zh.lua
@@ -5,165 +5,165 @@
 -- Translated by:
 -- ImRyckDer
 
-SafeAddString(LBOOKS_QUEST_BOOK									= "Book available through a quest", 1)
-SafeAddString(LBOOKS_MAYBE_NOT_HERE							= "[Book is maybe not here]", 1)
-SafeAddString(LBOOKS_QUEST_IN_ZONE							= "Quest in <<1>>", 1)
+SafeAddString(LBOOKS_QUEST_BOOK,					"Book available through a quest", 1)
+SafeAddString(LBOOKS_MAYBE_NOT_HERE,				"[Book is maybe not here]", 1)
+SafeAddString(LBOOKS_QUEST_IN_ZONE,					"Quest in <<1>>", 1)
 
 -- Thank You for datamining
 
-SafeAddString(LBOOKS_THANK_YOU1									= "Thank you", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG1						= "You're the first to find this lost book", 1)
+SafeAddString(LBOOKS_THANK_YOU1,					"谢谢你", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG1,				"你是第一个找到这本丢失的书的人", 1)
 
-SafeAddString(LBOOKS_THANK_YOU10								= "Keep it up!", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG10						= "And we'll get all those old books recorded", 1)
+SafeAddString(LBOOKS_THANK_YOU10,					"保持下去！", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG10,				"那我们就可以收录所有的旧书籍", 1)
 
-SafeAddString(LBOOKS_THANK_YOU20								= "Librarian", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG20						= "By the way, did you read those books ? You should", 1)
+SafeAddString(LBOOKS_THANK_YOU20,					"图书管理员", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG20,				"順道一提，你有读过那些书吗？你应该讀的", 1)
 
-SafeAddString(LBOOKS_THANK_YOU30								= "Keeper of the Elders Scrolls", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG30						= "It could be a nice title for you", 1)
+SafeAddString(LBOOKS_THANK_YOU30,					"Keeper of the Elders Scrolls", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG30,				"It could be a nice title for you", 1)
 
 -- Congratulations for collecting
 
-SafeAddString(LBOOKS_THANK_YOU2500							= "Already ?", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG2500					= "You found 2500 books over Tamriel", 1)
+SafeAddString(LBOOKS_THANK_YOU2500,					"Already ?", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG2500,			"You found 2500 books over Tamriel", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3000							= "Another type of achievement", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3000					= "You really wanted to collect those 3000 books", 1)
+SafeAddString(LBOOKS_THANK_YOU3000,					"Another type of achievement", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3000,			"You really wanted to collect those 3000 books", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3100							= "All those books read", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3100					= "Consider writing one too", 1)
+SafeAddString(LBOOKS_THANK_YOU3100,					"All those books read", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3100,			"Consider writing one too", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3200							= "Seeker of Knowledge", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3200					= "You made the longest", 1)
+SafeAddString(LBOOKS_THANK_YOU3200,					"Seeker of Knowledge", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3200,			"You made the longest", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3300							= "Path of the wisdom", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3300					= "Sill a bit more books and you won't be forgotten", 1)
+SafeAddString(LBOOKS_THANK_YOU3300,					"Path of the wisdom", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3300,			"Sill a bit more books and you won't be forgotten", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3350							= "Awakened Mind", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3350					= "Take your time to enjoy", 1)
+SafeAddString(LBOOKS_THANK_YOU3350,					"Awakened Mind", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3350,			"Take your time to enjoy", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3400							= "Only a few more", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3400					= "And you'll achieve something great", 1)
+SafeAddString(LBOOKS_THANK_YOU3400,					"Only a few more", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3400,			"And you'll achieve something great", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3450							= "I maybe didn't found all books yet", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3450					= "But it's definitively on my list", 1)
+SafeAddString(LBOOKS_THANK_YOU3450,					"I maybe didn't found all books yet", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3450,			"But it's definitively on my list", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3500							= "Too many findings", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3500					= "Bookshelve is too small !", 1)
+SafeAddString(LBOOKS_THANK_YOU3500,					"Too many findings", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3500,			"Bookshelve is too small !", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3550							= "Another one !", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3550					= "And you already know where the missings are", 1)
+SafeAddString(LBOOKS_THANK_YOU3550,					"Another one !", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3550,			"And you already know where the missings are", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3600							= "Three, Two, One ...", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3600					= "The Final Countdown", 1)
+SafeAddString(LBOOKS_THANK_YOU3600,					"Three, Two, One ...", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3600,			"The Final Countdown", 1)
 
-SafeAddString(LBOOKS_THANK_YOU3622							= "Congratulations", 1)
-SafeAddString(LBOOKS_THANK_YOU_LONG3622					= "There is no one else like you", 1)
+SafeAddString(LBOOKS_THANK_YOU3622,					"Congratulations", 1)
+SafeAddString(LBOOKS_THANK_YOU_LONG3622,			"There is no one else like you", 1)
 
 --tooltips
-SafeAddString(LBOOKS_KNOWN											= "Collected", 1)
+SafeAddString(LBOOKS_KNOWN,							"已收集", 1)
 
-SafeAddString(LBOOKS_MOREINFO1									= "Town", 1)
-SafeAddString(LBOOKS_MOREINFO2									= "Delve", 1)
-SafeAddString(LBOOKS_MOREINFO3									= "Public Dungeon", 1)
-SafeAddString(LBOOKS_MOREINFO4									= "Underground", 1)
-SafeAddString(LBOOKS_MOREINFO5									= "Group Instance", 1)
+SafeAddString(LBOOKS_MOREINFO1,						"城镇", 1)
+SafeAddString(LBOOKS_MOREINFO2,						"Delve", 1)
+SafeAddString(LBOOKS_MOREINFO3,						"Public Dungeon", 1)
+SafeAddString(LBOOKS_MOREINFO4,						"Underground", 1)
+SafeAddString(LBOOKS_MOREINFO5,						"Group Instance", 1)
 
 --settings menu header
-SafeAddString(LBOOKS_TITLE											= "LoreBooks", 1)
+SafeAddString(LBOOKS_TITLE,							"典籍", 1)
 
 --appearance
-SafeAddString(LBOOKS_PIN_TEXTURE								= "Select map pin icons", 1)
-SafeAddString(LBOOKS_PIN_TEXTURE_EIDETIC				= "Select map pin icons (<<1>>)", 1)
-SafeAddString(LBOOKS_PIN_TEXTURE_DESC						= "Select map pin icons.", 1)
-SafeAddString(LBOOKS_PIN_SIZE										= "Pin size", 1)
-SafeAddString(LBOOKS_PIN_SIZE_DESC							= "Set the size of the map pins.", 1)
-SafeAddString(LBOOKS_PIN_LAYER									= "Pin layer", 1)
-SafeAddString(LBOOKS_PIN_LAYER_DESC							= "Set the layer of the map pins", 1)
+SafeAddString(LBOOKS_PIN_TEXTURE,					"选择地图图钉图案", 1)
+SafeAddString(LBOOKS_PIN_TEXTURE_EIDETIC,			"选择地图图钉图案 (<<1>>)", 1)
+SafeAddString(LBOOKS_PIN_TEXTURE_DESC,				"选择地图图钉图案。", 1)
+SafeAddString(LBOOKS_PIN_SIZE,						"图钉大小", 1)
+SafeAddString(LBOOKS_PIN_SIZE_DESC,					"设置地图图钉的大小。", 1)
+SafeAddString(LBOOKS_PIN_LAYER,						"图钉图层", 1)
+SafeAddString(LBOOKS_PIN_LAYER_DESC,				"设置地图图钉的图层", 1)
 
 --compass
-SafeAddString(LBOOKS_COMPASS_UNKNOWN						= "Show lorebooks on the compass.", 1)
-SafeAddString(LBOOKS_COMPASS_UNKNOWN_DESC				= "Show/Hide icons for unknown lorebooks on the compass.", 1)
-SafeAddString(LBOOKS_COMPASS_DIST								= "Max pin distance", 1)
-SafeAddString(LBOOKS_COMPASS_DIST_DESC					= "The maximum distance for pins to appear on the compass.", 1)
+SafeAddString(LBOOKS_COMPASS_UNKNOWN,				"在罗盘上显示典籍。", 1)
+SafeAddString(LBOOKS_COMPASS_UNKNOWN_DESC,			"在罗盘上显示/隐藏未知的典籍的图标。", 1)
+SafeAddString(LBOOKS_COMPASS_DIST,					"最大图钉距离", 1)
+SafeAddString(LBOOKS_COMPASS_DIST_DESC,				"图钉出现在罗盘上的最大距离。", 1)
 
 --filters
-SafeAddString(LBOOKS_UNKNOWN										= "Show unknown lorebooks", 1)
-SafeAddString(LBOOKS_UNKNOWN_DESC								= "Show/Hide icons for unknown lorebooks on the map.", 1)
-SafeAddString(LBOOKS_COLLECTED									= "Show already collected lorebooks", 1)
-SafeAddString(LBOOKS_COLLECTED_DESC							= "Show/Hide icons for already collected lorebooks on the map.", 1)
+SafeAddString(LBOOKS_UNKNOWN,						"显示未知的典籍", 1)
+SafeAddString(LBOOKS_UNKNOWN_DESC,					"在地图上显示/隐藏未知的典籍的图标。", 1)
+SafeAddString(LBOOKS_COLLECTED,						"显示已收集的典籍", 1)
+SafeAddString(LBOOKS_COLLECTED_DESC,				"在地图上显示/隐藏已收集的典籍的图标。", 1)
 
-SafeAddString(LBOOKS_SHARE_DATA									= "Share your discoveries with LoreBooks author", 1)
-SafeAddString(LBOOKS_SHARE_DATA_DESC						= "Enabling this option will share your discoveries with LoreBooks author by sending automatically an ingame mail with data collected.\nThis option is only available for EU Users, even if data collected is shared with NA ones\nPlease note that you may encounter a small lag with your skills when the mail is sent. Mail is silently sent every 30 books read.", 1)
+SafeAddString(LBOOKS_SHARE_DATA,					"与LoreBooks作者分享你的发现", 1)
+SafeAddString(LBOOKS_SHARE_DATA_DESC,				"启用此选项将會与LoreBooks作者共享你的發現，藉由使用游戏內置的邮件自动发送收集的数据。\n此选项仅适用于EU伺服器玩家，即使收集的数据与與NA伺服器共享。\n请注意，你的技能可能会遇到一些小延迟。每隔30本书就会默默地发送一次邮件。", 1)
 
-SafeAddString(LBOOKS_EIDETIC										= "Show unknown Eidetic Memory", 1)
-SafeAddString(LBOOKS_EIDETIC_DESC								= "Show/Hide unknown Eidetic Memory scrolls on map. Those scrolls are lore-related scrolls not involved into Mages Guild Progression, but only informative about Tamriel", 1)
-SafeAddString(LBOOKS_EIDETIC_COLLECTED					= "Show known Eidetic Memory", 1)
-SafeAddString(LBOOKS_EIDETIC_COLLECTED_DESC			= "Show/Hide known Eidetic Memory scrolls on map. Those scrolls are lore-related scrolls not involved into Mages Guild Progression, but only informative about Tamriel", 1)
+SafeAddString(LBOOKS_EIDETIC,						"显示未知的永恒记忆", 1)
+SafeAddString(LBOOKS_EIDETIC_DESC,					"在地图上显示/隐藏未知的永恒记忆卷轴。那些卷轴是与传说相关的卷轴，是与法师公会进展無關的，只提供有关泰姆瑞爾的信息。", 1)
+SafeAddString(LBOOKS_EIDETIC_COLLECTED,				"显示已知的永恒记忆", 1)
+SafeAddString(LBOOKS_EIDETIC_COLLECTED_DESC,		"在地图上显示/隐藏已知的永恒记忆卷轴。那些卷轴是与传说相关的卷轴，是与法师公会进展無關的，只提供有关泰姆瑞爾的信息。", 1)
 
-SafeAddString(LBOOKS_COMPASS_EIDETIC						= "Show unknown Eidetic Memory on compass", 1)
-SafeAddString(LBOOKS_COMPASS_EIDETIC_DESC				= "Show/Hide unknown Eidetic Memory scrolls on compass. Those scrolls are lore-related scrolls not involved into Mages Guild Progression, but only informative about Tamriel", 1)
+SafeAddString(LBOOKS_COMPASS_EIDETIC,				"在罗盘上显示未知的永恒记忆", 1)
+SafeAddString(LBOOKS_COMPASS_EIDETIC_DESC,			"在地图上显示/隐藏未知的永恒记忆卷轴。那些卷轴是与传说相关的卷轴，是与法师公会进展無關的，只提供有关泰姆瑞爾的信息。", 1)
 
-SafeAddString(LBOOKS_UNLOCK_EIDETIC							= "Unlock Eidetic Library", 1)
-SafeAddString(LBOOKS_UNLOCK_EIDETIC_DESC				= "This will unlock Eidetic Library even if you haven't done the Mage Guild questline. This option is only valid for EN/FR/DE users.", 1)
+SafeAddString(LBOOKS_UNLOCK_EIDETIC,				"解锁永恒图书馆", 1)
+SafeAddString(LBOOKS_UNLOCK_EIDETIC_DESC,			"這會解鎖永恆圖書館，即使你沒有完成法師公會任务链。此选项仅对EN/FR/DE玩家有效。", 1)
 
 --worldmap filters
-SafeAddString(LBOOKS_FILTER_UNKNOWN							= "Unknown lorebooks", 1)
-SafeAddString(LBOOKS_FILTER_COLLECTED						= "Collected lorebooks", 1)
-SafeAddString(LBOOKS_FILTER_EICOLLECTED					= "<<1>> (Collected)" , 1)
+SafeAddString(LBOOKS_FILTER_UNKNOWN,				"未知的典籍", 1)
+SafeAddString(LBOOKS_FILTER_COLLECTED,				"收集的典籍", 1)
+SafeAddString(LBOOKS_FILTER_EICOLLECTED,			"<<1>> (已收集)" , 1)
 
-SafeAddString(LBOOKS_SEARCH_LABEL								= "Search in the lore library :", 1)
-SafeAddString(LBOOKS_SEARCH_PLACEHOLDER					= "Lorebook Name", 1)
+SafeAddString(LBOOKS_SEARCH_LABEL,					"Search in the lore library :", 1)
+SafeAddString(LBOOKS_SEARCH_PLACEHOLDER,			"Lorebook Name", 1)
 
-SafeAddString(LBOOKS_RANDOM_POSITION						= "[Bookshelves]", 1)
+SafeAddString(LBOOKS_RANDOM_POSITION,				"[Bookshelves]", 1)
 
 -- Report
 
-SafeAddString(LBOOKS_REPORT_KEYBIND_RPRT				= "Report", 1)
-SafeAddString(LBOOKS_REPORT_KEYBIND_SWITCH			= "Switch Mode", 1)
-SafeAddString(LBOOKS_REPORT_KEYBIND_COPY				= "Copy", 1)
+SafeAddString(LBOOKS_REPORT_KEYBIND_RPRT,			"Report", 1)
+SafeAddString(LBOOKS_REPORT_KEYBIND_SWITCH,			"Switch Mode", 1)
+SafeAddString(LBOOKS_REPORT_KEYBIND_COPY,			"Copy", 1)
 
-SafeAddString(LBOOKS_RS_FEW_BOOKS_MISSING				= "Few books are still missing in the Shalidor Library...", 1)
-SafeAddString(LBOOKS_RS_MDONE_BOOKS_MISSING			= "You maxed Mages Guild Skillline ! But few books are still missing", 1)
-SafeAddString(LBOOKS_RS_GOT_ALL_BOOKS						= "You collected all Shalidor Library. Congratulations !", 1)
+SafeAddString(LBOOKS_RS_FEW_BOOKS_MISSING,			"Few books are still missing in the Shalidor Library...", 1)
+SafeAddString(LBOOKS_RS_MDONE_BOOKS_MISSING,		"You maxed Mages Guild Skillline ! But few books are still missing", 1)
+SafeAddString(LBOOKS_RS_GOT_ALL_BOOKS,				"You collected all Shalidor Library. Congratulations !", 1)
 
-SafeAddString(LBOOKS_RE_FEW_BOOKS_MISSING				= "Few books are still missing in the Eidetic Memory...", 1)
-SafeAddString(LBOOKS_RE_GOT_ALL_BOOKS						= "You collected all Eidetic Memory. Congratulations !", 1)
-SafeAddString(LBOOKS_RE_THREESHOLD_ERROR				= "You need to collect few more books in order to get a report on Eidetic Memory ...", 1)
+SafeAddString(LBOOKS_RE_FEW_BOOKS_MISSING,			"Few books are still missing in the Eidetic Memory...", 1)
+SafeAddString(LBOOKS_RE_GOT_ALL_BOOKS,				"You collected all Eidetic Memory. Congratulations !", 1)
+SafeAddString(LBOOKS_RE_THREESHOLD_ERROR,			"You need to collect few more books in order to get a report on Eidetic Memory ...", 1)
 
-SafeAddString(SafeAddString(LBOOKS_REPORT_BOOK	= "Report Book <<1>> ..", 1)
-SafeAddString(LBOOKS_EIDETIC_REPORT							= "Thank you for reporting corrections for LoreBooks.\nIf you want to contribute just please enable sharing in Addon Settings.\nIf you want to correct a specific pin, it's the correct place.\nPlease also be sure to have last version of the add-on (Your version: <<1>>)\n\nMy report :", 1)
+SafeAddString(LBOOKS_REPORT_BOOK,					"Report Book <<1>> ...", 1)
+SafeAddString(LBOOKS_EIDETIC_REPORT,				"Thank you for reporting corrections for LoreBooks.\nIf you want to contribute just please enable sharing in Addon Settings.\nIf you want to correct a specific pin, it's the correct place.\nPlease also be sure to have last version of the add-on (Your version: <<1>>)\n\nMy report :", 1)
 
-SafeAddString(LBOOKS_QUEST_PLACEHOLDER					= "Please type the quest name", 1)
-SafeAddString(LBOOKS_QUEST_LABEL								= "Quest name:", 1)
+SafeAddString(LBOOKS_QUEST_PLACEHOLDER,				"Please type the quest name", 1)
+SafeAddString(LBOOKS_QUEST_LABEL,					"Quest name:", 1)
 
-SafeAddString(LBOOKS_RS_NOT_HERE_ANYMORE				= "Book is not here anymore", 1)
-SafeAddString(LBOOKS_RS_NOT_HERE_ANYMORE_DESC		= "With time, some books are moved. I'm absolutly sure this book is not here anymore (Searched few minutes, upstairs, downstairs, maybe a little grotto)", 1)
-SafeAddString(LBOOKS_RS_NOT_HERE_ANYMORE_ERR		= "This book has been found recently (Horns of the Reach). Please search a bit better.", 1)
+SafeAddString(LBOOKS_RS_NOT_HERE_ANYMORE,			"Book is not here anymore", 1)
+SafeAddString(LBOOKS_RS_NOT_HERE_ANYMORE_DESC,		"With time, some books are moved. I'm absolutly sure this book is not here anymore (Searched few minutes, upstairs, downstairs, maybe a little grotto)", 1)
+SafeAddString(LBOOKS_RS_NOT_HERE_ANYMORE_ERR,		"This book has been found recently (Horns of the Reach). Please search a bit better.", 1)
 
-SafeAddString(SafeAddString(LBOOKS_RE_ELIGIBLE	= "This book has been found in Horns of the Reach (Sept 2017) Eidetic Memory at this exact location. You can report it", 1)
-SafeAddString(LBOOKS_RE_NOT_ELIGIBLE						= "This book has not yet been found in Horns of the Reach (Sept 2017) Eidetic Memory at this exact location. You may not report it. If you don't find the book, please ensure you may have the quest linked. The book may also have been moved elsewhere.", 1)
+SafeAddString(LBOOKS_RE_ELIGIBLE,					"This book has been found in Horns of the Reach (Sept 2017) Eidetic Memory at this exact location. You can report it", 1)
+SafeAddString(LBOOKS_RE_NOT_ELIGIBLE,				"This book has not yet been found in Horns of the Reach (Sept 2017) Eidetic Memory at this exact location. You may not report it. If you don't find the book, please ensure you may have the quest linked. The book may also have been moved elsewhere.", 1)
 
-SafeAddString(LBOOKS_RS_BELONG_QUEST						= "Book belong to a quest", 1)
-SafeAddString(LBOOKS_RS_BELONG_QUEST_DESC				= "This book is only available before/while/after a quest.", 1)
+SafeAddString(LBOOKS_RS_BELONG_QUEST,				"Book belong to a quest", 1)
+SafeAddString(LBOOKS_RS_BELONG_QUEST_DESC,			"This book is only available before/while/after a quest.", 1)
 
-SafeAddString(LBOOKS_RS_IN_DUNGEON							= "Book in a dungeon", 1)
-SafeAddString(LBOOKS_RS_IN_DUNGEON_DESC					= "This book is in a little dungeon or in a cave & this one don't have map.", 1)
+SafeAddString(LBOOKS_RS_IN_DUNGEON,					"Book in a dungeon", 1)
+SafeAddString(LBOOKS_RS_IN_DUNGEON_DESC,			"This book is in a little dungeon or in a cave & this one don't have map.", 1)
 
-SafeAddString(LBOOKS_SEND_DATA									= "Send my correction", 1)
+SafeAddString(LBOOKS_SEND_DATA,						"Send my correction", 1)
 
-SafeAddString(LBOOKS_REPORT_THANK_YOU						= "Thank you", 1)
-SafeAddString(LBOOKS_REPORT_THANK_YOU_SEC				= "Your modification will be checked and added in next version", 1)
+SafeAddString(LBOOKS_REPORT_THANK_YOU,				"Thank you", 1)
+SafeAddString(LBOOKS_REPORT_THANK_YOU_SEC,			"Your modification will be checked and added in next version", 1)
 
 -- Immersive Mode
-SafeAddString(LBOOKS_IMMERSIVE									= "Enable Immersive Mode based on", 1)
-SafeAddString(LBOOKS_IMMERSIVE_DESC							= "Unknown Lorebooks won't be displayed based on the completion of the following objective on the current zone you are looking at", 1)
+SafeAddString(LBOOKS_IMMERSIVE,						"启用沉浸模式基于...", 1)
+SafeAddString(LBOOKS_IMMERSIVE_DESC,				"根据你所查看的当前区域的以下任務的完成情况，将不会显示未知的典籍", 1)
 
-SafeAddString(LBOOKS_IMMERSIVE_CHOICE1	= "Disabled", 1)
-SafeAddString(LBOOKS_IMMERSIVE_CHOICE2	= "Zone Main Quest", 1)
-SafeAddString(LBOOKS_IMMERSIVE_CHOICE5	= "Zone Quests", 1)
+SafeAddString(LBOOKS_IMMERSIVE_CHOICE1,				"禁用", 1)
+SafeAddString(LBOOKS_IMMERSIVE_CHOICE2,				"区域主線任务", 1)
+SafeAddString(LBOOKS_IMMERSIVE_CHOICE5,				"区域任务", 1)
 
 -- Quest Books
-SafeAddString(LBOOKS_USE_QUEST_BOOKS		= "Use Quest Books (Beta)", 1)
-SafeAddString(LBOOKS_USE_QUEST_BOOKS_DESC	= "Will try to use quest tools when they are received to avoid missing inventory-only books. May also use things like maps because there's no distinction between books and other usable quest items.", 1)
+SafeAddString(LBOOKS_USE_QUEST_BOOKS,				"使用任务书（测试版）", 1)
+SafeAddString(LBOOKS_USE_QUEST_BOOKS_DESC,			"将在收到任务时尝试使用任务工具，以避免錯過仅限物品欄的书籍。也可能使用像地图之类的东西，因为书籍和其他可用的任务物品之间没有区别。", 1)
